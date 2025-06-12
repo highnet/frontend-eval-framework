467938e2c59d57783a8a6d1ffd2b1e1a7f5270b3
Joaquin Telleria <joaquintelleria@gmail.com>
2025-06-12 16:13:10 +0200

    refactor: remove installation setup snippet for wiener melange components

--- app/ui-components/snippets/wiener-melange-install-setup.js
+++ app/ui-components/snippets/wiener-melange-install-setup.js
@@ -1,4 +1,1 @@
-export const wienerMelangeInstallSetup = `<!-- Include via CDN -->
-<script src="https://cdn.jsdelivr.net/npm/@wiener-melange/components"></script>
-
-<!-- Or install via npm -->`;
+export const wienerMelangeInstallSetup = `?`;
