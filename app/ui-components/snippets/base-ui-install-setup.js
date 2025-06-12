b0ff041b744a01afd660a8875acaf9cd8be5cc6d
Joaquin Telleria <joaquintelleria@gmail.com>
2025-06-12 16:13:44 +0200

    refactor: streamline installation setup snippets for base UI and MUI components

--- app/ui-components/snippets/base-ui-install-setup.js
+++ app/ui-components/snippets/base-ui-install-setup.js
@@ -1,4 +1,2 @@
-export const baseUiInstallSetup = `npm install @base_ui/react
-
-// No theme setup needed - you control all styling
+export const baseUiInstallSetup = `// No theme setup needed - you control all styling
 import { Button } from '@base_ui/react/Button'`;
--- app/ui-components/snippets/mui-install-setup.js
+++ app/ui-components/snippets/mui-install-setup.js
@@ -1,5 +1,2 @@
-export const muiInstallSetup = `npm install @mui/material @emotion/react
-
-// Theme setup in layout
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+export const muiInstallSetup = `import { ThemeProvider, createTheme } from '@mui/material/styles'
 const theme = createTheme({ /* config */ })`;
